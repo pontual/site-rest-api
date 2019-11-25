3c7c0d35b9d9786cd2541f88bb91e5f34d76c598
Heitor Chang <heitorchang@gmail.com>
2019-11-25 11:06:56 -0300

    fetch all produtos

--- public_html/api/index.php
+++ public_html/api/index.php
@@ -1,4 +1,7 @@
 <?php
+require_once("../../settings.php");
+require_once("../../db_cls.php");
+
 require __DIR__ . '/../../vendor/autoload.php';
 
 use Psr\Http\Message\ResponseInterface as Response;
@@ -8,11 +11,24 @@ use Slim\Factory\AppFactory;
 $app = AppFactory::create();
 $app->setBasePath('/api');
 
+
 $app->get('/', function (Request $request, Response $response, $args) {
   $data = ['version' => '0.0.1'];
   $payload = json_encode($data);
-  
+
   $response->getBody()->write($payload);
+  return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/produtos/', function (Request $request, Response $response, $args) {
+  $pdowrapper = new PdoWrapper();
+  $dbh = $pdowrapper->getConnection();
+
+  $sth = $dbh->prepare("select codigo, descricao, peso, medidas, preco from v2_produto");
+  $sth->execute();
+  
+  $response->getBody()->write(json_encode($sth->fetchAll()));
+  
   return $response->withHeader('Content-Type', 'application/json');
 });
 
