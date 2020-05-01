78381d20e164f68a27f47024603b12eb9e93fcee
Heitor Chang <heitorchang@gmail.com>
2020-05-01 17:47:42 -0300

    react client

---
+++ catalogo_client/reactapp/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ProdutoList from './ProdutoList.js';
+
+function App() {
+  return (
+    <div className="App">
+      <ProdutoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ catalogo_client/reactapp/src/ProdutoList.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import { ClienteAuth } from './sitesecrets.js';
+
+function ProdutoCard(props) {
+    return (
+        <div key={props.codigo}>
+        {props.codigo} {props.descricao}
+        </div>
+    );
+}
+
+
+class ProdutoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            produtos: [],
+        };
+    }
+
+    render() {
+        return (
+            <div>
+            {this.state.produtos.map((val, idx) => {
+                return <ProdutoCard codigo={val.codigo} descricao={val.descricao} />
+            })}
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        axios.get("http://pontualimportbrindes.com.br/api/produtos/", {auth: ClienteAuth})
+             .then(res => {
+                 const rows = res.data;
+                 this.setState({ produtos: rows });
+             });
+    }
+
+}
+
+export default ProdutoList;
